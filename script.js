aae04e0f5dd2242a8d0795ba7e765479181d04d8
Emil <emilv.kjellin@gmail.com>
2024-03-05 20:30:46 +0100

    javan är klar då jag lagt till så att varor kan läggas till och tas bort, samt att det finns en maximal limit på varor som får finnas i arrayen.

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-function lägg_till_varukorg(namn, pris)
+let korg = [];
+
+function lägg_till_varukorg(vara_namn, vara_pris)
 {
     let vara = 
     {
-        vara_namn:namn
-        //vara_pris:pris
+        namn: vara_namn,
+        pris: vara_pris
+        
+       
+    };
+    korg.push(vara);
+    visa_varukorg();
+}
+function visa_varukorg()
+{
+    let korgElement = document.getElementById('korg');
+    korgElement.innerHTML = '';
+
+    korg.forEach(function(vara) {
+        let varaNamn = document.createElement('h4');
+        varaNamn.textContent = vara.namn;
+        let varaPris = document.createElement('p');
+        varaPris.textContent = vara.pris;
+        korgElement.appendChild(varaNamn);
+        korgElement.appendChild(varaPris);
+    });
+    if (korg.length === 5)
+    {
+        korg.length = 4;
     }
-    const produkter = [];
-    produkter[0]="proteinbitar";
-    produkter[1]="proteinpulver";
+}
+function tömm_varukorg()
+{
+    korg.length = 0;
+    visa_varukorg();
 }
